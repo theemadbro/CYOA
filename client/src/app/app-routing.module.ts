401c5c3f686590a7349d6db0ea47c75864792f8d
Kevin Capotosto <foexzya@yahoo.com>
2018-05-21 14:23:18 -0700

    reupload routing setup

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BoardComponent } from './board/board.component'
+import { HomeComponent } from './home/home.component'
+
+const routes: Routes = [
+	{path: '', component: HomeComponent},
+	{path: 'board:id', component: BoardComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
