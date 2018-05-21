d2a4bb0e98e1c7118a9cb724b0ffbc079fa8922f
Robocop278 <37610945+Robocop278@users.noreply.github.com>
2018-05-21 16:27:03 -0700

    Initial Create
    Initial creation of Node and Story schemas, possibility of providing images in nodes considered but not included as of FVP implementation

--- server/models.js
+++ server/models.js
@@ -1,6 +1,21 @@
 var mongoose = require('mongoose');
+
 var CYOAschema = new mongoose.Schema({
 	title: {type:String, required: true},
 	description: {type:String, default: ""},
 }, {timestamps: true})
 mongoose.model('CYOA', CYOAschema)
+
+var Nodeschema = new mongoose.Schema({
+	content: {type:String, required: true},
+	note: {type:String, required: false},
+	//consider adding functionality of including images or other features to each node
+})
+mongoose.model('Node', Nodeschema)
+
+var Storyschema = new mongoose.Schema({
+	title: {type:String, required: true},
+	transitionTable: {type:[[Number]], required: true},
+	nodeList: {type:[Nodeschema], default: mongoose.model('Node', Nodeschema) },
+}, {timestamps: true})
+mongoose.model('Story', Storyschema)
