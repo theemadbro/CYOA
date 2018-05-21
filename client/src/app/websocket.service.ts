9e91dd91af0e70ca2bfdc4750568ba48d75607ed
Kevin Capotosto <foexzya@yahoo.com>
2018-05-21 16:49:06 -0700

    starting CRUD / progress on chat and socket.io

--- client/src/app/websocket.service.ts
+++ client/src/app/websocket.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs/';
+import * as Rx from 'rxjs/Rx';
+import { environment } from '../environments/environment'
+
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class WebsocketService {
 
-  constructor() { }
+	private socket;
+
+	constructor() { }
+
+	connect(): Rx.Subject<MessageEvent> {
+		this.socket = io(environment.ws_url)
+
+		let observable = new Observable(observer => {
+			this.socket.on('message', (data) => {
+				console.log("Recieved a message from websocket server")
+				observer.next(data);
+			})
+			return () => {
+				this.socket.disconnect();
+			}
+		})
+		
+		let observer = {
+			next: (data: Object) => {
+				this.socket.emit('message', JSON.stringify(data));
+			},
+		}
+		return Rx.Subject.create(observer, observable);
+	}
 }
