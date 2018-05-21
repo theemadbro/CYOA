3ccfb4b8f057188a534264aba5d7511f92fd7c33
Robocop278 <37610945+Robocop278@users.noreply.github.com>
2018-05-21 16:58:03 -0700

    Updated Story and Node schemas
    Added defaults to certain fields in Story and Node schemas, take note to create a defined variable or enumerator to indicate -1 as a flag for the transition table

--- server/models.js
+++ server/models.js
@@ -7,15 +7,15 @@ var CYOAschema = new mongoose.Schema({
 mongoose.model('CYOA', CYOAschema)
 
 var Nodeschema = new mongoose.Schema({
-	content: {type:String, required: true},
+	content: {type:String, default: "Enter text here", required: true},
 	note: {type:String, required: false},
 	//consider adding functionality of including images or other features to each node
 })
 mongoose.model('Node', Nodeschema)
 
 var Storyschema = new mongoose.Schema({
-	title: {type:String, required: true},
-	transitionTable: {type:[[Number]], required: true},
-	nodeList: {type:[Nodeschema], default: mongoose.model('Node', Nodeschema) },
+	title: {type:String, default: "Enter your title here", required: true},
+	transitionTable: {type:[[Number]], default: [[-1,-1,-1]], required: true}, //-1 will indicate the lack of a valid path from that option, this should be defined elsewhere and then updated here
+	nodeList: {type:[Nodeschema], default: mongoose.model('Node', Nodeschema)},
 }, {timestamps: true})
 mongoose.model('Story', Storyschema)
