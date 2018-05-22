bb334cc9f697b0ccdd077b8a43d6e07dad682d20
Robocop278 <37610945+Robocop278@users.noreply.github.com>
2018-05-21 18:09:26 -0700

    Initial declaration of Story and Node post/delete
    created declarations for posting and deletion of Story and Node models

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const mongoose = require('mongoose'),
 	story = mongoose.model('Story'),
-	storynode = mongoose.model('Node')
+	node = mongoose.model('Node')
 module.exports = function(app) {
+
+	//create new story
 	app.post('/story/new', function(req,res) {
 		var pack = {
 			state: 'good',
@@ -28,6 +30,59 @@ module.exports = function(app) {
 			}
 		})
 	})
+
+	//delete story
+	app.delete('/story/:id', function(req, res) {
+		story.deleteOne({_id: req.params.id}, function(err, singleData) {
+			if (err) {
+				console.log(err)
+				res.json(err)
+			}
+			// Will return a message of if the action went ok
+			res.json(singleData)
+		})
+	}) 
+
+	//create node
+	app.post('/node/new', function(req,res) {
+		var pack = {
+			state: 'good',
+			data: ''
+		}
+		console.log('creating new node')
+		var newnode = new node({
+			content: req.body.content,
+			note: req.body.note
+		})
+		newnode.save(function(err, data){
+			if (err) {
+				console.log('POST/CREATION ERRORS!')
+				pack['state'] = 'bad'
+				pack['data'] = err
+				res.json(pack)
+			}
+			else {
+				console.log('POST SUCCESS!')
+				pack['data'] = data
+				res.json(pack)
+				// res.redirect('/pets/'+data._id)
+			}
+		})
+	})
+
+	//delete node
+	app.delete('/node/:id', function(req, res) {
+		node.deleteOne({_id: req.params.id}, function(err, singleData) {
+			if (err) {
+				console.log(err)
+				res.json(err)
+			}
+			// Will return a message of if the action went ok
+			res.json(singleData)
+		})
+	}) 
+
+	//catch angular routes
 	app.all("**", (req, res, next) => {
 		console.log('route hits!')
 		res.sendFile(path.resolve("./client/dist/client/index.html"))
