1dd01a3c326210971f4dbce5175561100862d1f2
ZoeKat <zoezoefchastain@gmail.com>
2018-05-23 13:18:37 -0700

    added html and css for view adv

---
+++ client/src/app/viewadv/viewadv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewadv',
+  templateUrl: './viewadv.component.html',
+  styleUrls: ['./viewadv.component.css']
+})
+export class ViewadvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/viewone/viewone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewone',
+  templateUrl: './viewone.component.html',
+  styleUrls: ['./viewone.component.css']
+})
+export class ViewoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
