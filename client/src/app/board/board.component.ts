2a42b3963ed3d0821268cb63dbe1d8bcff7f4066
Kevin Capotosto <foexzya@yahoo.com>
2018-05-24 15:31:49 -0700

    testestestestsetse

--- client/src/app/board/board.component.ts
+++ client/src/app/board/board.component.ts
@@ -33,4 +33,85 @@ export class BoardComponent implements OnInit {
 			}
 		})
 	}
+	public list = [
+    {
+      title: 'childless',
+      children: []
+    },
+    {
+      title: 'great grandparent',
+      children: [
+        {
+          title: 'childless grandsibiling',
+          children: []
+        },
+        {
+          title: 'grandparent',
+          children: [
+            {
+              title: 'childless sibiling',
+              children: []
+            },
+            {
+              title: 'another childless sibiling',
+              children: []
+            },
+            {
+              title: 'parent',
+              children: [
+                {
+                  title: 'child',
+                  children: []
+                },
+                {
+                  title: 'another child',
+                  children: []
+                },
+              ]
+            },
+            {
+              title: 'another parent',
+              children: [
+                {
+                  title: 'child',
+                  children: []
+                },
+              ]
+            },
+          ]
+        },
+        {
+          title: 'another grandparent',
+          children: [
+            {
+              title: 'parent',
+              children: [
+                {
+                  title: 'child',
+                  children: []
+                },
+                {
+                  title: 'another child',
+                  children: []
+                },
+                {
+                  title: 'a third child',
+                  children: []
+                },
+                {
+                  title: 'teen mother',
+                  children: [
+                    {
+                      title: 'accident',
+                      children: []
+                    },
+                  ]
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    },
+];
 }
